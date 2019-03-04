3b3767b5ff11ce4119b57ed4bf15476785bb8bd1
Dustin J. Mitchell <dustin@mozilla.com>
2019-03-04 17:33:39 +0000

    No bug: capture absolute path for --ci-configuration-directory r=tomprince
    The `ci-admin check` command changes working directory, so it broke use of
    `--ci-configuration-directory`.
    
    Differential Revision: https://phabricator.services.mozilla.com/D21959

--- ciadmin/generate/ciconfig/__init__.py
+++ ciadmin/generate/ciconfig/__init__.py
@@ -4,6 +4,7 @@
 # v. 2.0. If a copy of the MPL was not distributed with this file, You can
 # obtain one at http://mozilla.org/MPL/2.0/.
 
+import os
 import click
 
 from ...options import decorate
@@ -24,6 +25,8 @@ def options(fn):
         ),
         click.option(
             "--ci-configuration-directory",
+            # capture the absolute path, in case the cwd changes later
+            callback=lambda c, p, value: os.path.abspath(value),
             help="local directory containing ci-configuration repository (overrides repository/revision)",
         ),
     )
