d7f2baf03b9434def9f1ce0c1b34725b98765a01
Dustin J. Mitchell <dustin@mozilla.com>
2019-10-14 21:16:40 +0000

    v1.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name="tc-admin",
-    version="1.1.0",
+    version="1.2.0",
     description="Administration of Taskcluster runtime configuration",
     long_description=long_description,
     long_description_content_type='text/markdown',
