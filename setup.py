1f9ca05bc253e72c4bbf460ef9316b5e3b5152f6
Dustin J. Mitchell <dustin@mozilla.com>
2019-11-03 02:27:58 +0000

    Specify a semver compatibility range for TC client
    The ~= operator's semantics are a bit surprising, and it cannot pin a
    patch version.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     url="https://github.com/taskcluster/tc-admin",
     packages=find_packages("."),
     install_requires=[
-        "taskcluster~=21.3.0",
+        "taskcluster~=21.3",
         "click~=7.0",
         "blessings~=1.7",
         "attrs~=19.3.0",
