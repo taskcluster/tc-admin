8004554e5fe8bd08b8e5d0b3fe9ddc68e3eb529c
Pete Moore <pmoore@mozilla.com>
2024-08-07 17:39:07 +0200

    v4.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name="tc-admin",
-    version="4.0.0",
+    version="4.0.1",
     description="Administration of Taskcluster runtime configuration",
     long_description=long_description,
     long_description_content_type='text/markdown',
